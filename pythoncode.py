d333c0f7e79ab4cedd0925ea240a21b873fc473c
Pablo Barroso González <pablobarrosogonzalez2@gmail.com>
2026-07-12 02:32:07 +0200

    añadiendo idioma

--- pythoncode.py
+++ pythoncode.py
@@ -1,3 +1,10 @@
 
-mensaje = "Hola, quiero crear este archivo de prueba para poder subir mi primer archivo a GitHub"
+idioma = input("What is your language?\n1- English\n2- Español\n")
+if idioma == "1":
+    mensaje = "Hello, I want to create this test file so I can upload my first file to GitHub."
+elif idioma == "2":
+    mensaje = "Hola, quiero crear este archivo de prueba para poder subir mi primer archivo a GitHub"
+else:
+    mensaje = "Invalid option"
+
 print(mensaje)
